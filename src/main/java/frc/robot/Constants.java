318466a44103cc14e36c99b5378f7cd400b4b085
s114194-ctrl <250770802+s114194-ctrl@users.noreply.github.com.>
2026-02-04 23:21:22 +0800

    nothing

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -2,7 +2,7 @@ package frc.robot;
 
 public final class Constants {
 
-    public static class IntakeConstants {
+    public static class IntakeContants {
         public static final int Intake_Roller_ID = 21;
         public static final int Intake_Ctrl_ID = 22;
 
@@ -10,33 +10,33 @@ public final class Constants {
         public static final double Intake_Out = 0;
         public static final double Intake_In = 0;
 
+        public static final double MAX_ACCEL = 500;
+        public static final double MAX_VELOCITY = 200;
+
         // RollerMT的PID
-        public static final double Roller_Out_P = 0.0001;
-        public static final double Roller_Out_I = 0.0001;
-        public static final double Roller_Out_D = 0.01;
-        public static final double Roller_Out_F = 0.00017;
+        public static final double Roller_Out_P = 0.4;
+        public static final double Roller_Out_I = 0;
+        public static final double Roller_Out_D = 0;
+        public static final double Roller_Out_F = 0;
 
-        public static final double Roller_Back_P = 0.0001;
-        public static final double Roller_Back_I = 0.0001;
-        public static final double Roller_Back_D = 0.01;
-        public static final double Roller_Back_F = 0.00017;
-
-        public static final double ROLLER_MAX_ACCEL = 2000;
-        public static final double ROLLER_MAX_VELOCITY = 1500;
+        public static final double Roller_Back_P = 0.4;
+        public static final double Roller_Back_I = 0;
+        public static final double Roller_Back_D = 0;
+        public static final double Roller_Back_F = 0;
 
         //
-        public static final double Intake_Out_P = 0.0001;
-        public static final double Intake_Out_I = 0.0001;
-        public static final double Intake_Out_D = 0.01;
-        public static final double Intake_Out_F = 0.00017;
+        public static final double Intake_Out_P = 0.4;
+        public static final double Intake_Out_I = 0;
+        public static final double Intake_Out_D = 0;
+        public static final double Intake_Out_F = 0;
 
-        public static final double Intake_Back_P = 0.0001;
-        public static final double Intake_Back_I = 0.0001;
-        public static final double Intake_Back_D = 0.01;
-        public static final double Intake_Back_F = 0.00017;
+        public static final double Intake_Back_P = 0.4;
+        public static final double Intake_Back_I = 0;
+        public static final double Intake_Back_D = 0;
+        public static final double Intake_Back_F = 0;
 
-        public static final double INTAKE_MAX_ACCEL = 2000;
-        public static final double INTAKE_MAX_VELOCITY = 1500;
+        public static final boolean Intake_ctrl_Inverted = true;
+        public static final boolean Intake_Roller_Inverted = false;
 
     }
 
@@ -46,11 +46,12 @@ public final class Constants {
         public static final int tubeMotor1_ID = 42; // 待改
         public static final int tubeMotor2_ID = 43; // 待改
 
-        // Climber Confi
-
-        public static final double Climb_Angle = -30;
+        // Climber Config
+        public static final boolean climberMotor_Inverted = false;
+        public static final boolean tubeMotor1_Inverted = false;
+        public static final boolean tubeMotor2_Inverted = true;
+        // public static final double Climb_Angle = -30;
         public static final double Climb_Zero = 178;
-
         public static final double Climb_StartUp = -24;
 
         // Climber PIDF
@@ -78,11 +79,6 @@ public final class Constants {
         public static final double MAX_ACCEL = 1000;
         public static final double MAX_VELOCITY = 400;
 
-        public static final double floor = 0; // 我還不知道怎麼填 先亂填
-        public static final double L1 = 0;
-        public static final double L2 = 0;
-        public static final double L3 = 0;
-
     }
 
     public static class ShooterConstants {
@@ -92,23 +88,26 @@ public final class Constants {
         public static final int superneo_ID = 46; // 待改
 
         // Shooter Config
-        public static final boolean BigFlyWheel_Inverted = false;
-        public static final boolean SmallFlyWheel_Inverted = false;
         public static final boolean superneo_Inverted = false;
         // public static final double Climb_Angle = -30;
-        public static final double Shooter_Zero = 178;
-        public static final double Shooter_StartUp = -24;
+        public static final double ShooterB_Zero = 0;
+        public static final double ShooterB_Out = 0;
+        public static final double ShooterB_Back = 0;
+
+        public static final double ShooterS_Zero = 0;
+        public static final double ShooterS_Out = 0;
+        public static final double ShooterS_Back = 0;
 
         // Shooter BigFlyWheel PIDF
-        public static final double Shooter_Out_P = 0;
-        public static final double Shooter_Out_I = 0;
-        public static final double Shooter_Out_D = 0;
-        public static final double Shooter_Out_F = 0;
+        public static final double Shooterb_Out_P = 0;
+        public static final double Shooterb_Out_I = 0;
+        public static final double Shooterb_Out_D = 0;
+        public static final double Shooterb_Out_F = 0;
 
-        public static final double Shooter_Back_P = 0;
-        public static final double Shooter_Back_I = 0;
-        public static final double Shooter_Back_D = 0;
-        public static final double Shooter_Back_F = 0;
+        public static final double Shooterb_Back_P = 0;
+        public static final double Shooterb_Back_I = 0;
+        public static final double Shooterb_Back_D = 0;
+        public static final double Shooterb_Back_F = 0;
 
         // Shooter SmallFlyWheel PIDF
         public static final double Shooters_Out_P = 0;
@@ -132,7 +131,8 @@ public final class Constants {
         public static final double suppernet_Back_D = 0;
         public static final double supperneo_Back_F = 0;
 
-        // 最大加速度,最大速度
+
+        //最大加速度,最大速度
         public static final double MAX_ACCEL = 1000;
         public static final double MAX_VELOCITY = 400;
 
@@ -159,7 +159,7 @@ public final class Constants {
         public static final double indexerMT_Back_D = 0;
         public static final double indexerMT_Back_F = 0;
 
-        // 最大加速度,最大速度
+        //最大加速度,最大速度
         public static final double MAX_ACCEL = 1000;
         public static final double MAX_VELOCITY = 400;
 
@@ -168,5 +168,4 @@ public final class Constants {
     public static class VisionConstants {
         public static final String LLName = "light";
     }
-
 }
