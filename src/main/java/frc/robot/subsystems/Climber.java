9f28a09995158faea566da553b8fd8e08b3fab56
s114194-ctrl <250770802+s114194-ctrl@users.noreply.github.com.>
2026-02-04 23:13:12 +0800

    修改部分void

--- src/main/java/frc/robot/subsystems/Climber.java
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -1,8 +1,7 @@
-
 package frc.robot.subsystems;
 
 import frc.robot.Constants.ClimberConstants;
-
+import frc.robot.Constants.IntakeConstants;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj.DriverStation;
 
@@ -11,105 +10,70 @@ import com.ctre.phoenix6.controls.Follower;
 import com.ctre.phoenix6.controls.MotionMagicDutyCycle;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.configs.Slot0Configs;
+import com.ctre.phoenix6.configs.Slot1Configs;
 import com.ctre.phoenix6.controls.MotionMagicVelocityVoltage;
 import com.ctre.phoenix6.signals.InvertedValue;
 
 public class Climber extends SubsystemBase {
 
-  private TalonFX climberMotor = new TalonFX(ClimberConstants.climberMotor_ID, "canbus");
-  private TalonFX tubeMotor1 = new TalonFX(ClimberConstants.tubeMotor1_ID, "canbus");
-  private TalonFX tubeMotor2 = new TalonFX(ClimberConstants.tubeMotor2_ID, "canbus");
-  private Follower follower;
-  TalonFXConfiguration configuration = new TalonFXConfiguration();
+  private final TalonFX climberMotor = new TalonFX(ClimberConstants.climberMotor_ID, "canbus");
+  private final TalonFX tubeMotor1 = new TalonFX(ClimberConstants.tubeMotor1_ID, "canbus");
+  private final TalonFX tubeMotor2 = new TalonFX(ClimberConstants.tubeMotor2_ID, "canbus");
 
   public Climber() {
-    // climberMotor.setPosition(0);
-    // 回歸原始設定
-    // climberMotor.setPosition(0);
-    // 回歸原始設定
+    TalonFXConfiguration climberConfig = new TalonFXConfiguration();
+    TalonFXConfiguration tubeConfig = new TalonFXConfiguration();
 
-    // var talonFXConfigs = new TalonFXConfiguration();
-    climberMotor.getConfigurator().apply(new TalonFXConfiguration());
-    tubeMotor1.getConfigurator().apply(new TalonFXConfiguration());
-    tubeMotor2.getConfigurator().apply(configuration);
-    tubeMotor2.setControl(follower.withLeaderID(1));
-    configuration.MotorOutput.Inverted = InvertedValue.CounterClockwise_Positive;
+    tubeConfig.MotorOutput.Inverted = InvertedValue.CounterClockwise_Positive;
 
-    var Climber_Ctrl_Config = climberMotor.getConfigurator();
-    Slot0Configs Climber_Out_PIDConfig = new Slot0Configs();
-    Climber_Out_PIDConfig.kP = ClimberConstants.Climber_Out_P;
-    Climber_Out_PIDConfig.kI = ClimberConstants.Climber_Out_I;
-    Climber_Out_PIDConfig.kD = ClimberConstants.Climber_Out_D;
-    Climber_Out_PIDConfig.kV = ClimberConstants.Climber_Out_F;
-    Climber_Ctrl_Config.apply(Climber_Out_PIDConfig);
+    climberConfig.Slot0.kP = ClimberConstants.Climber_Angle_P;
+    climberConfig.Slot0.kI = ClimberConstants.Climber_Angle_I;
+    climberConfig.Slot0.kD = ClimberConstants.Climber_Angle_D;
+    climberConfig.Slot0.kV = ClimberConstants.Climber_Angle_F;
 
-    Slot0Configs Climber_Back_PIDConfig = new Slot0Configs();
-    Climber_Back_PIDConfig.kP = ClimberConstants.Climber_Back_P;
-    Climber_Back_PIDConfig.kI = ClimberConstants.Climber_Back_I;
-    Climber_Back_PIDConfig.kD = ClimberConstants.Climber_Back_D;
-    Climber_Back_PIDConfig.kV = ClimberConstants.Climber_Back_F;
-    Climber_Ctrl_Config.apply(Climber_Back_PIDConfig);
+    climberConfig.Slot1.kP = ClimberConstants.Line_Out_P;
+    climberConfig.Slot1.kI = ClimberConstants.Line_Out_I;
+    climberConfig.Slot1.kD = ClimberConstants.Line_Out_D;
+    climberConfig.Slot1.kV = ClimberConstants.Line_Out_F;
 
-    var Line_Ctrl_Config = climberMotor.getConfigurator();
-    Slot0Configs Line_Out_PIDConfig = new Slot0Configs();
-    Line_Out_PIDConfig.kP = ClimberConstants.Line_Out_P;
-    Line_Out_PIDConfig.kI = ClimberConstants.Line_Out_I;
-    Line_Out_PIDConfig.kD = ClimberConstants.Line_Out_D;
-    Line_Out_PIDConfig.kV = ClimberConstants.Line_Out_F;
-    Line_Ctrl_Config.apply(Line_Out_PIDConfig);
+    climberMotor.getConfigurator().apply(climberConfig);
+    tubeMotor1.getConfigurator().apply(tubeConfig);
+    tubeMotor2.getConfigurator().apply(tubeConfig);
 
-    Slot0Configs Line_Back_PIDConfig = new Slot0Configs();
-    Line_Back_PIDConfig.kP = ClimberConstants.Line_Back_P;
-    Line_Back_PIDConfig.kI = ClimberConstants.Line_Back_I;
-    Line_Back_PIDConfig.kD = ClimberConstants.Line_Back_D;
-    Line_Back_PIDConfig.kV = ClimberConstants.Line_Back_F;
-    Line_Ctrl_Config.apply(Line_Back_PIDConfig);
+    tubeMotor2.setControl(new Follower(tubeMotor1.getDeviceID(), false));
 
     if (DriverStation.isEnabled()) {
-      climberMotor.setControl(new MotionMagicDutyCycle(ClimberConstants.Climb_StartUp));
+      climberMotor.setControl(new MotionMagicDutyCycle(ClimberConstants.Climb_Zero));
     }
   }
 
   public void Climb_Zero() {
-    climberMotor.setControl(new MotionMagicVelocityVoltage(ClimberConstants.Climb_Zero));
-    tubeMotor1.setControl(new MotionMagicVelocityVoltage(ClimberConstants.Climb_Zero));
+    climberMotor.setControl(new MotionMagicVelocityVoltage(ClimberConstants.Climb_Zero).withSlot(0));
+    tubeMotor1.setControl(new MotionMagicVelocityVoltage(ClimberConstants.Climb_Zero).withSlot(0));
   }
 
-  public void Climb() {
-    climberMotor.setControl(new MotionMagicVelocityVoltage(ClimberConstants.Climb_Angle));
+  public void Climb_Angle() {
+    climberMotor.setControl(new MotionMagicDutyCycle(IntakeConstants.Intake_Out));
   }
 
-  // Command
-  public void outing() {
-    tubeMotor1.set(0.6);
+  public void Line_out() {
+    climberMotor.setControl(new MotionMagicDutyCycle(IntakeConstants.Intake_Out).withSlot(1));
   }
 
-  public void backing() {
+  public void Line_Back() {
     tubeMotor1.set(-0.6);
   }
 
-  public void holding() {
-    climberMotor.set(0.4);
-  }
-
-  public void opening() {
-    climberMotor.set(-0.4);
-  }
-
-  // 停止收線
   public void stopTube() {
     tubeMotor1.set(0);
   }
 
-  // 停止勾吊掛的桿子
   public void stopClimber() {
     climberMotor.set(0);
   }
 
-  // 停止所有的馬達(永久)(遊戲停止時)
   public void stopAll() {
     climberMotor.stopMotor();
     tubeMotor1.stopMotor();
   }
-
 }
