120041215b43ebe285df6a833184499a50887737
s114194-ctrl <250770802+s114194-ctrl@users.noreply.github.com.>
2026-02-05 11:03:45 +0800

    climber latest

--- src/main/java/frc/robot/subsystems/Climber.java
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -1,116 +1,104 @@
-
 package frc.robot.subsystems;
 
+
+import frc.robot.Constants.ClimberConstants;
+import frc.robot.Constants.IntakeConstants;
+import frc.robot.Constants;
+
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj.DriverStation;
+
 
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.controls.Follower;
+import com.ctre.phoenix6.controls.MotionMagicDutyCycle;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.configs.Slot0Configs;
-//import com.ctre.phoenix6.signals.InvertedValue;
-
-import edu.wpi.first.math.filter.SlewRateLimiter;
-import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.Commands;
-import frc.robot.Constants.ClimberConstants;
-
-
+import com.ctre.phoenix6.configs.Slot1Configs;
+import com.ctre.phoenix6.controls.MotionMagicVelocityVoltage;
+import com.ctre.phoenix6.signals.InvertedValue;
+import com.ctre.phoenix6.signals.MotorAlignmentValue;
 
 
 public class Climber extends SubsystemBase {
-  private TalonFX climberMotor = new TalonFX(ClimberConstants.climberMotor_ID ,"canbus");
-  private TalonFX tubeMotor1 = new TalonFX(ClimberConstants.tubeMotor1_ID ,"canbus");
-  private TalonFX tubeMotor2 = new TalonFX(ClimberConstants.tubeMotor2_ID ,"canbus");
-  private Follower follower ;
 
-  private final SlewRateLimiter climberOutLimiter = new SlewRateLimiter(0.5);
-  private final SlewRateLimiter climberBackLimiter = new SlewRateLimiter(-0.5);
-  private final SlewRateLimiter rollerOutLimiter = new SlewRateLimiter(0.5);
-  private final SlewRateLimiter rollerBackLimiter = new SlewRateLimiter(-0.333333);
 
-  public void Init() {
-    //var talonFXConfigs = new TalonFXConfiguration();
-    climberMotor.getConfigurator().apply(new TalonFXConfiguration());
-    tubeMotor1.getConfigurator().apply(new TalonFXConfiguration());
-    tubeMotor2.getConfigurator().apply(new TalonFXConfiguration());
-    tubeMotor2.setControl(follower.withLeaderID(1));
-    //(可能要反轉)config2.MotorOutput.Inverted = InvertedValue.Clockwise_Positive;
-        
-    //climberMotor.setPosition(0);
-    //回歸原始設定         
-    //climberMotor.setPosition(0);
-    //回歸原始設定  
+  private final TalonFX climberMotor = new TalonFX(ClimberConstants.climberMotor_ID, Constants.CANIVORE_BUS);
+  private final TalonFX tubeMotor1 = new TalonFX(ClimberConstants.tubeMotor1_ID, Constants.CANIVORE_BUS);
+  private final TalonFX tubeMotor2 = new TalonFX(ClimberConstants.tubeMotor2_ID, Constants.CANIVORE_BUS);
 
-    var Climber_Ctrl_Config = climberMotor.getConfigurator();
-    Slot0Configs Climber_Out_PIDConfig =new Slot0Configs();
-        Climber_Out_PIDConfig.kP = ClimberConstants.Climber_Out_P;
-        Climber_Out_PIDConfig.kI = ClimberConstants.Climber_Out_I;
-        Climber_Out_PIDConfig.kD = ClimberConstants.Climber_Out_D;
-        Climber_Out_PIDConfig.kV = ClimberConstants.Climber_Out_F;
-        Climber_Ctrl_Config.apply(Climber_Out_PIDConfig);
 
-        Slot0Configs Climber_Back_PIDConfig =new Slot0Configs();
-        Climber_Back_PIDConfig.kP = ClimberConstants.Climber_Back_P;
-        Climber_Back_PIDConfig.kI = ClimberConstants.Climber_Back_I;
-        Climber_Back_PIDConfig.kD = ClimberConstants.Climber_Back_D;
-        Climber_Back_PIDConfig.kV = ClimberConstants.Climber_Back_F;
-        Climber_Ctrl_Config.apply(Climber_Back_PIDConfig);
-        //設定來回的PID值(依齒輪比決定)
+  public Climber() {
+    TalonFXConfiguration climberConfig = new TalonFXConfiguration();
+    TalonFXConfiguration tubeConfig = new TalonFXConfiguration();
 
-    
-    
-    var Line_Ctrl_Config = climberMotor.getConfigurator();
-    Slot0Configs Line_Out_PIDConfig =new Slot0Configs();
-        Line_Out_PIDConfig.kP = ClimberConstants.Line_Out_P;
-        Line_Out_PIDConfig.kI = ClimberConstants.Line_Out_I;
-        Line_Out_PIDConfig.kD = ClimberConstants.Line_Out_D;
-        Line_Out_PIDConfig.kV = ClimberConstants.Line_Out_F;
-        Line_Ctrl_Config.apply(Line_Out_PIDConfig);
 
-        Slot0Configs Line_Back_PIDConfig =new Slot0Configs();
-        Line_Back_PIDConfig.kP = ClimberConstants.Line_Back_P;
-        Line_Back_PIDConfig.kI = ClimberConstants.Line_Back_I;
-        Line_Back_PIDConfig.kD = ClimberConstants.Line_Back_D;
-        Line_Back_PIDConfig.kV = ClimberConstants.Line_Back_F;
-        Line_Ctrl_Config.apply(Line_Back_PIDConfig);
-        //設定來回的PID值(依齒輪比決定)
+    tubeConfig.MotorOutput.Inverted = InvertedValue.CounterClockwise_Positive;
+
+
+    climberConfig.Slot0.kP = ClimberConstants.Climber_Angle_P;
+    climberConfig.Slot0.kI = ClimberConstants.Climber_Angle_I;
+    climberConfig.Slot0.kD = ClimberConstants.Climber_Angle_D;
+    climberConfig.Slot0.kV = ClimberConstants.Climber_Angle_F;
+
+
+    climberConfig.Slot1.kP = ClimberConstants.Line_Out_P;
+    climberConfig.Slot1.kI = ClimberConstants.Line_Out_I;
+    climberConfig.Slot1.kD = ClimberConstants.Line_Out_D;
+    climberConfig.Slot1.kV = ClimberConstants.Line_Out_F;
+
+
+    climberMotor.getConfigurator().apply(climberConfig);
+    tubeMotor1.getConfigurator().apply(tubeConfig);
+    tubeMotor2.getConfigurator().apply(tubeConfig);
+
+
+    tubeMotor2.setControl(new Follower(tubeMotor1.getDeviceID(), MotorAlignmentValue.Aligned));
+
+
+    if (DriverStation.isEnabled()) {
+      climberMotor.setControl(new MotionMagicDutyCycle(ClimberConstants.Climb_Zero));
+    }
   }
 
-  public Command Climber_Out(double speed) {
-    return Commands.runOnce(
-        () -> climberMotor.set(climberOutLimiter.calculate(speed)), this);
-  }//勾住
 
-  public Command Climber_Back(double speed) {
-    return Commands.runOnce(
-        () -> climberMotor.set(climberBackLimiter.calculate(speed)), this);
-  }//放開
-
-
-  public Command Line_Out(double speed) {
-        return Commands.sequence(
-      Commands.run(() -> {
-      tubeMotor1.set(rollerOutLimiter.calculate(speed));
-      }, this),
-      Commands.waitSeconds(2),  // 暫定
-      Commands.run(() -> {
-        tubeMotor1.set(0);
-      }, this)
-    );
-    // 捲線
+  public void Climb_Zero() {
+    climberMotor.setControl(new MotionMagicVelocityVoltage(ClimberConstants.Climb_Zero).withSlot(0));
+    tubeMotor1.setControl(new MotionMagicVelocityVoltage(ClimberConstants.Climb_Zero).withSlot(0));
   }
 
-  public Command Line_back(double speed) {
-      return Commands.sequence(
-      Commands.run(() -> {
-        tubeMotor1.set(rollerBackLimiter.calculate(speed));
-      }, this),
-      Commands.waitSeconds(3),  // 暫定
-      Commands.run(() -> {
-        tubeMotor1.set(0);
-      }, this)
-    );
-      // 放線
-      //慢
+
+  public void Climb_Angle() {
+    climberMotor.setControl(new MotionMagicDutyCycle(IntakeConstants.Intake_Out));
+  }
+
+
+  public void Line_out() {
+    climberMotor.setControl(new MotionMagicDutyCycle(IntakeConstants.Intake_Out).withSlot(1));
+  }
+
+
+  public void Line_Back() {
+    tubeMotor1.set(-0.6);
+  }
+
+
+  public void stopTube() {
+    tubeMotor1.set(0);
+  }
+
+
+  public void stopClimber() {
+    climberMotor.set(0);
+  }
+
+
+  public void stopAll() {
+    climberMotor.stopMotor();
+    tubeMotor1.stopMotor();
   }
 }
+
+
+
