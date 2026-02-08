0be5313cb66ecafc0f58b1392d641f079dc2ef6d
Allen <49893966+Alllen95Wei@users.noreply.github.com>
2026-02-08 15:24:39 +0800

    fix broken git :(

--- src/main/java/frc/robot/command/Intake/Intakein.java
+++ src/main/java/frc/robot/command/Intake/Intakein.java
@@ -12,7 +12,6 @@ public class Intakein extends SequentialCommandGroup {
   public Intakein(Intake intake) {
 
     addCommands(
-
      new RunCommand(() ->intake.back(), intake).withTimeout(3),
      new InstantCommand(() ->intake.intake_dont_do_that(), intake),
      new InstantCommand(() ->intake.stopRoller(), intake));
