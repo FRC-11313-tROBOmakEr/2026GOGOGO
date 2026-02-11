60c2de22c2f6e25eb2dffa9a394ff169bfca137e
Allen <49893966+Alllen95Wei@users.noreply.github.com>
2026-02-12 02:15:26 +0800

    test shootout

--- src/main/java/frc/robot/command/shoot/Shootout.java
+++ src/main/java/frc/robot/command/shoot/Shootout.java
@@ -15,24 +15,24 @@ import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 
-
-
-
-
 public class Shootout extends SequentialCommandGroup {
   public PointWheelsAt point = new PointWheelsAt();
   public Target target = new Target();
+
   public Shootout(Shooter shooter) {
 
     addRequirements(shooter);
     addCommands(
-    // new InstantCommand(() -> target.getTargetStatus(LimelightHelpers.getFiducialID(VisionConstants.LLName))),
-    //new RunCommand(() -> swerve.applyRequest(() -> point.withModuleDirection(target.getAimingRotation(LimelightHelpers.getBotPose2d(VisionConstants.LLName))))),
-    new InstantCommand(() -> shooter.Shooter_Out(),shooter),
-    Commands.waitSeconds(0.8),
-    new InstantCommand(() -> shooter.indexerRun()),
-    new InstantCommand(() -> shooter.conveyorRun())
-        
+      Commands.print("Shootout running"),
+        // new InstantCommand(() ->
+        // target.getTargetStatus(LimelightHelpers.getFiducialID(VisionConstants.LLName))),
+        // new RunCommand(() -> swerve.applyRequest(() ->
+        // point.withModuleDirection(target.getAimingRotation(LimelightHelpers.getBotPose2d(VisionConstants.LLName))))),
+        new InstantCommand(() -> shooter.Shooter_Out(), shooter),
+        Commands.waitSeconds(0.8),
+        new InstantCommand(() -> shooter.indexerRun()),
+        new InstantCommand(() -> shooter.conveyorRun())
+
     );
   }
 }
